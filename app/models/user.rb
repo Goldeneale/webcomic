8b3f68838930c81378f8948c40a5a76d31a3b25a
Christopher Mamuscia <goldenwing42@gmail.com>
2017-06-14 08:29:30 +0000

    Added hovertext and title functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  after_initialize :init
+         
+  def init
+    self.rank ||= "commoner"
+  end
 end
