2abb83ef66d47a3ec629e7077861028acd3ead4e
Christopher Mamuscia <goldenwing42@gmail.com>
2017-06-14 01:41:05 +0000

    Created prototype panel upload form

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -5,4 +5,21 @@ class PanelsController < ApplicationController
   def new
     @panel = Panel.new
   end
+  
+  def create
+    @panel = Panel.new(panel_params)
+    if @panel.save
+      flash[:success] = "Successfully saved panel to database."
+      redirect_to new_panel_path
+    else
+      flash[:danger] = @panel.errors.full_messages.join(", and")
+      redirect_to new_panel_path
+    end
+  end
+  
+  private
+    def panel_params
+      params.require(:panel).permit(:title, :panel, :page, :width, :height, :hovertext, :comment)
+    end
+    
 end
